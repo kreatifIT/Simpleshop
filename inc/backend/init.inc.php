37e8004aa5a2e40fa17a75a9a064176c9f7b6fe4
Alex Platter <a.platter@kreatif.it>
2017-03-15 14:52:51 +0100

    assets copying modified

--- inc/backend/init.inc.php
+++ inc/backend/init.inc.php
@@ -15,17 +15,17 @@ namespace FriendsOfREDAXO\Simpleshop;
 
 \rex::setProperty('simpleshop.product_variants', 'variants');
 
-\rex_extension::register('PACKAGES_INCLUDED', function ()
-{
-    if ($this->getProperty('compile'))
-    {
+\rex_extension::register('PACKAGES_INCLUDED', function () {
+    if ($this->getProperty('compile') || \rex_addon::get('project')->getProperty('compile') || !file_exists($this->getAssetsPath('css/backend.css'))) {
         $compiler   = new \rex_scss_compiler();
         $scss_files = \rex_extension::registerPoint(new \rex_extension_point('BE_STYLE_SCSS_FILES', [$this->getPath('assets/styles.scss')]));
         $compiler->setScssFile($scss_files);
         $compiler->setCssFile($this->getPath('assets/styles.css'));
         $compiler->compile();
         \rex_file::copy($this->getPath('assets/styles.css'), $this->getAssetsPath('css/styles.css'));
+        \rex_file::delete($this->getPath('assets/css/styles.css'));
     }
+
+    // CSS
+    \rex_view::addCssFile($this->getAssetsUrl('css/styles.css?mtime=' . filemtime($this->getAssetsPath('css/styles.css'))));
 });
-// CSS
-\rex_view::addCssFile($this->getAssetsUrl('css/styles.css?mtime='. filemtime($this->getAssetsPath('css/styles.css'))));
