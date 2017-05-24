c11a2979dee0acedc5aeaa982dc4ae52d59578d6
Alex Platter <a.platter@kreatif.it>
2017-05-24 15:35:06 +0200

    model-data logic updated

--- lib/yform/value/model_data.php
+++ lib/yform/value/model_data.php
@@ -17,13 +17,23 @@ class rex_yform_value_model_data extends rex_yform_value_textarea
     {
         parent::enterObject();
 
-        if (rex::isBackend())
-        {
-            $value      = $this->getValue();
-            $Object     = \FriendsOfREDAXO\Simpleshop\Model::unprepare($value);
-            $content    = $this->unprepare($Object);
-            $attributes = $this->getAttributeElements(["name" => $this->getFieldName()]);
+        if (rex::isBackend()) {
+            $value       = $this->getValue();
+            $Object      = \FriendsOfREDAXO\Simpleshop\Model::unprepare($value);
+            $content     = $this->unprepare($Object);
+            $attributes  = $this->getAttributeElements(["name" => $this->getFieldName()]);
+            $content_cnt = count($content);
 
+            if ($content_cnt <= 2) {
+                if ($content_cnt <= 1) {
+                    $content = ['<table class="table table-condensed">'];
+                }
+                else if ($content_cnt == 2) {
+                    array_pop($content);
+                }
+                $content[] = '<tr><td><i class="text-muted">'. rex_i18n::msg('list_no_rows') .'</i></td></tr>';
+                $content[] = '</table>';
+            }
             $this->params['form_output'][$this->getId()] = '
                 <div class="form-group" id="' . $this->getHTMLId() . '">
                     <label for="' . $this->getFieldId() . '">' . $this->getLabel() . '</label>
@@ -36,58 +46,47 @@ class rex_yform_value_model_data extends rex_yform_value_textarea
 
     private function unprepare($Object)
     {
-        $close_table = TRUE;
+        $close_table = true;
         $content     = ['<table class="table table-condensed">'];
 
-        if (!is_object($Object) && is_array($Object))
-        {
-            foreach ($Object as $key => $val)
-            {
-                if (!is_numeric($key) && in_array ($key, ['submit']))
-                {
+        if (!is_object($Object) && is_array($Object)) {
+            foreach ($Object as $key => $val) {
+                if (!is_numeric($key) && in_array($key, ['submit'])) {
                     continue;
                 }
                 else {
-                    $content[] = '<tr><td>' . (is_numeric($key) ? 'index: '. $key : $key) . '</td><td>' . implode('', $this->unprepare($val)) . '</td></tr>';
+                    $content[] = '<tr><td>' . (is_numeric($key) ? 'index: ' . $key : $key) . '</td><td>' . implode('', $this->unprepare($val)) . '</td></tr>';
                 }
             }
         }
-        else if (is_object($Object) && defined(get_class($Object) . "::TABLE"))
-        {
+        else if (is_object($Object) && defined(get_class($Object) . "::TABLE")) {
             $columns = \rex_yform_manager_table::get($Object::TABLE)->getFields();
 
-            foreach ($columns as $column)
-            {
-                if (in_array($column->getType(), ['validate']) || in_array($column->getTypeName(), ['php', 'html', 'hidden_input']))
-                {
+            foreach ($columns as $column) {
+                if (in_array($column->getType(), ['validate']) || in_array($column->getTypeName(), ['php', 'html', 'hidden_input'])) {
                     continue;
                 }
                 $value = $Object->getValue($column->getName());
 
-                if (is_array($value))
-                {
+                if (is_array($value)) {
                     $value = implode('', $this->unprepare($value));
                 }
                 $content[] = '<tr><td>' . $column->getLabel() . '</td><td>' . $value . '</td></tr>';
             }
         }
-        else if (is_object($Object))
-        {
+        else if (is_object($Object)) {
             $data = $Object->getData();
 
-            foreach ($data as $name => $value)
-            {
+            foreach ($data as $name => $value) {
                 $content[] = '<tr><td>' . $name . '</td><td>' . implode('', $this->unprepare($value)) . '</td></tr>';
             }
         }
-        else
-        {
+        else {
             $content     = [$Object];
-            $close_table = FALSE;
+            $close_table = false;
         }
 
-        if ($close_table)
-        {
+        if ($close_table) {
             $content[] = '</table>';
         }
         return $content;
