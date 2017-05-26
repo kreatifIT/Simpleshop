f444af442eab71b70a5eadd554de9c51574b575b
Alex Platter <a.platter@kreatif.it>
2017-05-26 12:01:20 +0200

    getAbo and verify method added

--- lib/models/Package.php
+++ lib/models/Package.php
@@ -16,4 +16,31 @@ namespace FriendsOfREDAXO\Simpleshop;
 class Package extends Model
 {
     const TABLE = 'rex_shop_package';
+
+    public function getAbo()
+    {
+        $Abo = null;
+
+        if ($this->verifyPackageValidity()) {
+            $Abo = PackageCategory::get($this->getValue('abo_id'));
+        }
+        return $Abo;
+    }
+
+    public function verifyPackageValidity()
+    {
+        $product_ids = $this->getArrayValue('products');
+
+        foreach ($product_ids as $product_id) {
+            $product = Product::get($product_id);
+            if (
+                !$product->isOnline()
+                || $product->getValue('amount') < 1
+                || \rex_extension::registerPoint(new \rex_extension_point('simpleshop.Package.verifyPackageValidity', false, ['Package' => $this, 'Product' => $product]))
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
