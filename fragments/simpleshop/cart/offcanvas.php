568cf05f40a54748ee07e7862bf9dbf1006d0bee
Johannes <j.ganthaler@kreatif.it>
2018-03-26 13:27:30 +0200

    offcanvas cart

--- fragments/simpleshop/cart/offcanvas.php
+++ fragments/simpleshop/cart/offcanvas.php
@@ -17,22 +17,27 @@ use Kreatif\Resource;
 $picture = '';
 
 ?>
-<div class="cart-aside">
-    <button type="button">###label.continue_shopping###</button>
-    <div class="cart">
-        <div class="cart-item">
-            <a class="cart-item-image" href="#">
+<div class="offcanvas-cart">
+    <button class="offcanvas-cart-continue-shopping" type="button">###label.continue_shopping###</button>
+    <div class="offcanvas-cart-success">
+        <span class="check">✔</span>
+        <span class="description">Der Artikel wurde erfolgreich in den Warenkorb gelegt</span>
+    </div>
+
+    <div class="offcanvas-cart-items">
+        <div class="offcanvas-cart-item">
+            <a class="image" href="#">
                 <?= Resource::getImgTag($picture, 'cart-list-element-main') ?>
             </a>
-            <div class="cart-item-description">
+            <div class="description">
                 <span class="amount">2x </span>
                 <span class="name">Lorem ipsum dolor sit amet.</span>
                 <span class="price">1400.00 €</span>
             </div>
-            <div class="cart-item-remove">x</div>
+            <div class="remove"></div>
         </div>
     </div>
-    <div class="cart-prices">
+    <div class="offcanvas-cart-prices">
         <div class="price">
             <span class="label">Zwischensumme</span>
             <span class="amount">1.631,08 €</span>
@@ -46,8 +51,8 @@ $picture = '';
             <span class="amount">1.775,07 €</span>
         </div>
     </div>
-    <div class="cart-buttons">
-        <a href="" class="button secondary">###action.proceed_to_checkout###</a>
-        <a href="" class="button secondary">###action.action.shop_edit_cart###</a>
+    <div class="offcanvas-cart-buttons">
+        <a href="" class="button secondary expanded margin-small-bottom">###action.proceed_to_checkout###</a>
+        <a href="" class="button expanded">###action.action.shop_edit_cart###</a>
     </div>
 </div>
