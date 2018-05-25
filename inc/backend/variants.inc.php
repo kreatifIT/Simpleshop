09d3418227c691588622d9d9a90982c5cd041c6c
Alex Platter <a.platter@kreatif.it>
2018-05-25 08:13:04 +0200

    credit note generation added

--- inc/backend/variants.inc.php
+++ inc/backend/variants.inc.php
@@ -21,14 +21,21 @@ namespace FriendsOfREDAXO\Simpleshop;
 
     if ($variant_type && $list_params['table_name'] == Product::TABLE)
     {
+        $url = \rex_url::backendPage('simpleshop/variants', [
+            'table_name' => Variant::TABLE,
+            'data_id' => '-ID-',
+            'func' => 'edit',
+        ]);
         $list->addColumn('variants', $this->i18n('action.manage_variants'), count($list->getColumnNames()) - 2);
         $list->setColumnLabel('variants', $this->i18n('label.variants'));
-        $list->setColumnParams('variants', [
-            'page'       => "simpleshop/{$variant_type}",
-            'func'       => 'edit',
-            'data_id'    => '###id###',
-            'table_name' => Variant::TABLE,
-        ]);
+        $list->setColumnFormat('variants', 'custom', function ($params) {
+            $Product = Product::get($params['list']->getValue('id'));
+
+            if ($Product->getValue('type') != 'package') {
+                $url = strtr($params['params']['url'], ['-ID-' => $params['list']->getValue('id')]);
+                return "<a href='{$url}'>{$params['subject']}</a>";
+            }
+        }, ['url' => $url]);
     }
     return $list;
 });
