6e3f5fd156b6ead403982d10a29311b5477eb88f
Alex Platter <alex.platter@gmail.com>
2018-07-26 14:39:25 +0200

    noindex bug fixed

--- lib/models/Product.php
+++ lib/models/Product.php
@@ -16,6 +16,7 @@ namespace FriendsOfREDAXO\Simpleshop;
 
 use Url\Url;
 
+
 class Product extends Model
 {
     const TABLE        = 'rex_shop_product';
@@ -52,8 +53,7 @@ class Product extends Model
             $includeTax = false;
             $extras     = $this->getValue('extras');
             $price      = $extras['price'];
-        }
-        else if ($use_reduced) {
+        } else if ($use_reduced) {
             $reduced = $this->getValue('reduced_price');
             $price   = $reduced > 0 ? $reduced : $price;
         }
@@ -67,25 +67,24 @@ class Product extends Model
         if (is_object($discount)) {
             if ($discount->getValue('discount_value') > 0) {
                 $price -= $discount->getValue('discount_value');
-            }
-            elseif ($discount->getValue('discount_percent') > 0) {
+            } else if ($discount->getValue('discount_percent') > 0) {
                 $price = $price / 100 * (100 - $discount->getValue('discount_percent'));
             }
         }
 
         if ($includeTax) {
-            $tax = Tax::get($this->getValue('tax'))->getValue('tax');
+            $tax = Tax::get($this->getValue('tax'))
+                ->getValue('tax');
 
             if ($Settings['brutto_prices']) {
                 $this->__tax = ($price / ($tax + 100)) * $tax;
-            }
-            else {
+            } else {
                 $this->__tax = $price * $tax * 0.01;
                 $price       = $price + $this->__tax;
             }
-        }
-        else if ($Settings['brutto_prices']) {
-            $tax = Tax::get($this->getValue('tax'))->getValue('tax');
+        } else if ($Settings['brutto_prices']) {
+            $tax = Tax::get($this->getValue('tax'))
+                ->getValue('tax');
 
             $this->__tax = ($price / ($tax + 100)) * $tax;
             $price       = $price - $this->__tax;
@@ -119,7 +118,9 @@ class Product extends Model
                 }
             }
             // get features
-            $_features = Feature::query()->where('id', $feature_ids)->find();
+            $_features = Feature::query()
+                ->where('id', $feature_ids)
+                ->find();
 
             foreach ($_features as $feature) {
                 // assign feature values to the feature
@@ -210,7 +211,10 @@ class Product extends Model
         }
         list ($product_id, $variant_key) = explode('|', trim($key, '|'));
 
-        $_this = self::query()->where('id', $product_id)->where('status', 1)->findOne();
+        $_this = self::query()
+            ->where('id', $product_id)
+            ->where('status', 1)
+            ->findOne();
 
         if (!$_this) {
             throw new ProductException("No product with ID = " . $product_id . " exists --key:{$key}", 1);
@@ -223,7 +227,11 @@ class Product extends Model
         $feature_ids = $variant_key ? explode(',', $variant_key) : [];
 
         if (count($feature_ids)) {
-            $variant = Variant::query()->where('product_id', $product_id)->where('type', 'NE', '!=')->where('variant_key', $variant_key)->findOne();
+            $variant = Variant::query()
+                ->where('product_id', $product_id)
+                ->where('type', 'NE', '!=')
+                ->where('variant_key', $variant_key)
+                ->findOne();
 
             if (!$variant) {
                 // the given combination does not exist!
@@ -235,30 +243,29 @@ class Product extends Model
         if ($_features && count($feature_ids)) {
             foreach ($feature_ids as $feature_id) {
                 // get variants
-                $feature = FeatureValue::query()->where('id', $feature_id)->where('status', 1)->findOne();
+                $feature = FeatureValue::query()
+                    ->where('id', $feature_id)
+                    ->where('status', 1)
+                    ->findOne();
 
                 if ($throwErrors) {
                     if (!$feature) {
                         throw new ProductException("No feature with ID = " . $feature_id . " exists --key:{$key}", 2);
-                    }
-                    else if ($_this->getValue('amount') <= 0) {
+                    } else if ($_this->getValue('amount') <= 0) {
                         // make some availabilty checks
                         throw new ProductException("Product not available any more --key:{$key}", 4);
-                    }
-                    else if ($_this->getValue('amount') < $_this->getValue('cart_quantity')) {
+                    } else if ($_this->getValue('amount') < $_this->getValue('cart_quantity')) {
                         throw new ProductException("Amount of product is lower than cart quantity --key:{$key}", 5);
                     }
                 }
                 $features[] = $feature;
             }
-        }
-        else {
+        } else {
             if ($throwErrors) {
                 if ($_this->getValue('amount') <= 0) {
                     // make some availabilty checks
                     throw new ProductException("Product not available any more --key:{$key}", 4);
-                }
-                else if ($_this->getValue('amount') < $_this->getValue('cart_quantity')) {
+                } else if ($_this->getValue('amount') < $_this->getValue('cart_quantity')) {
                     throw new ProductException("Amount of product is lower than cart quantity --key:{$key}", 5);
                 }
             }
@@ -271,16 +278,14 @@ class Product extends Model
     {
         foreach ($variant_data as $key => $value) {
             if (in_array($key, ['price', 'reduced_price', 'width', 'height', 'weight', 'length'])) {
-                if ((float) $value > 0) {
-                    $this->setValue($key, (float) $value);
+                if ((float)$value > 0) {
+                    $this->setValue($key, (float)$value);
                 }
-            }
-            elseif ($key == 'amount') {
+            } else if ($key == 'amount') {
                 if ($value != '') {
                     $this->setValue($key, $value);
                 }
-            }
-            elseif ($value != '' && !in_array($key, ['id', 'product_id', 'type'])) {
+            } else if ($value != '' && !in_array($key, ['id', 'product_id', 'type'])) {
                 $this->setValue($key, $value);
             }
         }
@@ -288,8 +293,7 @@ class Product extends Model
             // if variant is free set price to zero
             $this->setValue('price', 0);
             $this->setValue('reduced_price', 0);
-        }
-        else if ((float) $variant_data['price'] > 0 && (float) $variant_data['reduced_price'] <= 0) {
+        } else if ((float)$variant_data['price'] > 0 && (float)$variant_data['reduced_price'] <= 0) {
             // if variant has a price
             $this->setValue('reduced_price', 0);
         }
@@ -305,7 +309,8 @@ class Product extends Model
             $parents = $category->getParentTree();
 
             foreach ($parents as $parent) {
-                $_paths[] = Url::getRewriter()->normalize($parent->getValue('name_' . $lang_id));
+                $_paths[] = Url::getRewriter()
+                    ->normalize($parent->getValue('name_' . $lang_id));
             }
         }
         $_paths[] = $path;
@@ -316,7 +321,9 @@ class Product extends Model
     {
         $result = $Ep->getSubject();
 
-        if ($result !== false && $Ep->getParam('table')->getTableName() == self::TABLE) {
+        if ($result !== false && $Ep->getParam('table')
+                ->getTableName() == self::TABLE
+        ) {
             // remove all related variants
             $obj_id = $Ep->getParam('data_id');
             $query  = "DELETE FROM " . Variant::TABLE . " WHERE product_id = {$obj_id}";
@@ -331,29 +338,16 @@ class Product extends Model
         $Object = $Ep->getSubject();
         $vkey   = trim(rex_get('vkey', 'string'));
 
-        if ($Ep->getParam('table') == self::TABLE) {
-            if ($vkey == '') {
-                $sql = \rex_sql::factory();
-                $sql->setQuery("
-                    SELECT variant_key 
-                    FROM " . Variant::TABLE . " 
-                    WHERE product_id = :pid 
-                    AND type != 'NE'
-                    ORDER BY prio ASC LIMIT 1", ['pid' => $Object->getId()]);
-                $vkey = @$sql->getValue('variant_key');
-            }
-            if ($vkey != '') {
-                try {
-                    $Object = $Object->getVariant($vkey);
+        if ($vkey != '' && $Ep->getParam('table') == self::TABLE) {
+            try {
+                $Object = $Object->getVariant($vkey);
 
-                    \rex_extension::register('YREWRITE_ROBOTS_TAG', function (\rex_extension_point $Ep) {
-                        return false;
-                    });
-                }
-                catch (ProductException $ex) {
-                    \rex_response::setStatus(404);
-                    $Object = false;
-                }
+                \rex_extension::register('YREWRITE_ROBOTS_TAG', function (\rex_extension_point $Ep) {
+                    return false;
+                });
+            } catch (ProductException $ex) {
+                \rex_response::setStatus(404);
+                $Object = false;
             }
         }
         return $Object;
