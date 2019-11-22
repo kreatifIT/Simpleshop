293df372976c69aa81af181814bf12043746335b
Alex Platter <alex.platter@gmail.com>
2019-11-22 15:48:04 +0100

    shipping_address_id install patch added

---
+++ install/db_migrations/2019-11-22 09-00-00__init.php
@@ -0,0 +1,43 @@
+<?php
+
+$sql = rex_sql::factory();
+$sql->setQuery('SET FOREIGN_KEY_CHECKS = 0');
+
+try {
+
+    rex_sql_table::get(\FriendsOfREDAXO\Simpleshop\Order::TABLE)
+        ->ensureColumn(new rex_sql_column('shipping_address_id', 'int(11)', false, null, null))
+        ->ensure();
+
+    $fieldId = $sql->getArray('SELECT id FROM rex_yform_field WHERE table_name = :table AND name = :field AND type_id = :type', [
+        'table' => \FriendsOfREDAXO\Simpleshop\Order::TABLE,
+        'field' => 'shipping_address_id',
+        'type'  => 'value',
+    ], PDO::FETCH_COLUMN)[0];
+
+    $sql->setTable('rex_yform_field');
+    $sql->setValue('table_name', \FriendsOfREDAXO\Simpleshop\Order::TABLE);
+    $sql->setValue('prio', 3);
+    $sql->setValue('type_id', 'value');
+    $sql->setValue('type_name', 'customer_address');
+    $sql->setValue('db_type', 'int');
+    $sql->setValue('list_hidden', 0);
+    $sql->setValue('search', 0);
+    $sql->setValue('name', 'shipping_address_id');
+    $sql->setValue('label', '###label.shipping_address###');
+    $sql->setValue('empty_value', '###label.shipping_address###');
+    $sql->setValue('empty_option', 0);
+
+    if ($fieldId) {
+        $sql->setWhere('id = :id', ['id' => $fieldId]);
+        $sql->update();
+    } else {
+        $sql->insert();
+    }
+
+    pr('migration done');
+}
+finally {
+    $sql = rex_sql::factory();
+    $sql->setQuery('SET FOREIGN_KEY_CHECKS = 1');
+}
--- lib/yform/value/customer_address.php
+++ lib/yform/value/customer_address.php
@@ -16,9 +16,6 @@ class rex_yform_value_customer_address extends rex_yform_value_abstract
 
     public function enterObject()
     {
-        $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
-        $this->params['value_pool']['sql'][$this->getName()]   = $this->getValue();
-
         if (rex::isBackend()) {
             if ($this->params['send'] == 1 && $this->getValue() != '') {
                 $address                                          = \FriendsOfREDAXO\Simpleshop\CustomerAddress::get($this->getValue());
@@ -26,6 +23,14 @@ class rex_yform_value_customer_address extends rex_yform_value_abstract
             }
             $this->params['form_output'][$this->getId()] = $this->parse('value.customer_address.tpl.php');
         }
+
+        if ($this->params['send'] == 1 && (int)$this->getElement('empty_option') == 0 && $this->getValue() == '') {
+            $this->params['warning'][$this->getId()] = $this->params['error_class'];
+            $this->params['warning_messages'][$this->getId()] = $this->getElement('empty_value');
+        }
+
+        $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
+        $this->params['value_pool']['sql'][$this->getName()]   = $this->getValue();
     }
 
     public static function getListValue($params)
@@ -51,9 +56,11 @@ class rex_yform_value_customer_address extends rex_yform_value_abstract
             'name'        => 'customer_address',
             'description' => 'Auswahl Kundenadresse',
             'values'      => [
-                'name'   => ['type' => 'name', 'label' => rex_i18n::msg("yform_values_defaults_name")],
-                'label'  => ['type' => 'text', 'label' => rex_i18n::msg('yform_values_defaults_label')],
-                'notice' => ['type' => 'text', 'label' => rex_i18n::msg('yform_values_defaults_notice')],
+                'name'         => ['type' => 'name', 'label' => rex_i18n::msg("yform_values_defaults_name")],
+                'label'        => ['type' => 'text', 'label' => rex_i18n::msg('yform_values_defaults_label')],
+                'notice'       => ['type' => 'text', 'label' => rex_i18n::msg('yform_values_defaults_notice')],
+                'empty_option' => ['type' => 'boolean', 'label' => rex_i18n::msg('yform_values_be_manager_relation_empty_option')],
+                'empty_value'  => ['type' => 'text', 'label' => rex_i18n::msg('yform_values_be_manager_relation_empty_value')],
             ],
         ];
     }
