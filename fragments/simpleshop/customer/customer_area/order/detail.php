b6d0e10b30fbed2d5ad7f7db3331761dbd2fc08e
Alex Platter <alex.platter@gmail.com>
2020-02-27 11:43:19 +0100

    order history detail navigation added

--- fragments/simpleshop/customer/customer_area/order/detail.php
+++ fragments/simpleshop/customer/customer_area/order/detail.php
@@ -14,12 +14,15 @@
 namespace FriendsOfREDAXO\Simpleshop;
 
 
-$Order = $this->getVar('Order');
+$Order    = $this->getVar('Order');
+$cur_area = $this->getVar('area');
 $order_id = $Order->getValue('id');
-$Config = FragmentConfig::getValue('checkout');
+$Config   = FragmentConfig::getValue('checkout');
 
 $products  = [];
-$_products = OrderProduct::query()->where('order_id', $order_id)->find();
+$_products = OrderProduct::query()
+    ->where('order_id', $order_id)
+    ->find();
 
 
 foreach ($_products as $product) {
@@ -29,16 +32,22 @@ foreach ($_products as $product) {
     $products[] = $_product;
 }
 
-
-?>
-    <h1 class="heading">###simpleshop.order_num### <?= $order_id ?></h1>
-    <?php
-
 $Config['has_coupons']        = false;
 $Config['has_summary_footer'] = false;
 
-$fragment = new \rex_fragment();
-$fragment->setVar('Order', $Order);
-$fragment->setVar('Config', $Config);
-$fragment->setVar('products', $Order->getProducts(false));
-echo $fragment->parse('simpleshop/checkout/summary/wrapper.php');
+?>
+<div class="order-content">
+    <div class="navigation margin-bottom">
+        <a href="<?= rex_getUrl(null, null, ['ctrl' => $cur_area]) ?>">&laquo; ###action.back_to_overview###</a>
+        <hr>
+    </div>
+    <h1 class="heading">###simpleshop.order_num### <?= $order_id ?></h1>
+
+    <?php
+    $fragment = new \rex_fragment();
+    $fragment->setVar('Order', $Order);
+    $fragment->setVar('Config', $Config);
+    $fragment->setVar('products', $Order->getProducts(false));
+    echo $fragment->parse('simpleshop/checkout/summary/wrapper.php');
+    ?>
+</div>
