7c4b510bdb9be13e63a523f1f2501634101c484f
Alex Platter <alex.platter@gmail.com>
2020-05-04 18:21:46 +0200

    installation table existance check bugfix

--- install/inc/2__tables.inc.php
+++ install/inc/2__tables.inc.php
@@ -18,10 +18,17 @@ $tablesets = glob(__DIR__ . '/../tableset/*.json');
 foreach ($tablesets as $filepath) {
     $filename  = basename($filepath);
     $tablename = str_replace('.json', '', $filename);
-    $table     = rex_sql_table::get($tablename);
 
-    if (!$table->exists()) {
-        $tableset = file_get_contents($filepath);
-        rex_yform_manager_table_api::importTablesets($tableset);
+    try {
+        $tableCols = rex_sql::showColumns($tablename);
+    } catch (rex_sql_exception $exception) {
+        $sqlEx = $exception->getSql();
+        // Error code 42S02 means: Table does not exist
+        if ($sqlEx && $sqlEx->getErrno() === '42S02') {
+            $tableset = file_get_contents($filepath);
+            rex_yform_manager_table_api::importTablesets($tableset);
+        } else {
+            throw $exception;
+        }
     }
 }
