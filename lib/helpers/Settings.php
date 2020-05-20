9e0ecf83f8602e2df1cc35119785f03de3134bc0
Alex Platter <alex.platter@gmail.com>
2020-05-20 14:40:12 +0200

    Settings class added

---
+++ lib/helpers/Settings.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Kreatif\Project package.
+ *
+ * @author Kreatif GmbH
+ * @author a.platter@kreatif.it
+ * Date: 20.05.20
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfREDAXO\Simpleshop;
+
+
+class Settings
+{
+    public static function getValue($key)
+    {
+        $settings = \rex::getConfig('simpleshop.Settings');
+        return $settings[$key];
+    }
+
+    public static function getArticle($key)
+    {
+        $linklist = self::getValue('linklist');
+        $article = isset($linklist[$key]) ? \rex_article::get($linklist[$key]) : null;
+
+        if ($article && !$article->isOnline()) {
+            $article = null;
+        }
+        return $article;
+    }
+}
