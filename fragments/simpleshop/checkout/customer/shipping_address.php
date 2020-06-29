5a4f985bcf60e70a025542013227269314550f80
Alex Platter <alex.platter@gmail.com>
2020-06-29 19:07:30 +0200

    invoice + shipping fragment

--- fragments/simpleshop/checkout/customer/shipping_address.php
+++ fragments/simpleshop/checkout/customer/shipping_address.php
@@ -13,89 +13,77 @@
 
 namespace FriendsOfREDAXO\Simpleshop;
 
-$this->setVar('btn_label', '###action.go_ahead###');
+$addresses    = $this->getVar('addresses', []);
+$orderAddress = $this->getVar('Address');
+$back_url     = $this->getVar('back_url');
+$errors       = $this->getVar('errors');
 
-$useShAddress      = $this->getVar('use_shipping_address', \rex_request('shipping_address_is_idem', 'int') == 1);
-$createNewShipping = $this->getVar('create_new_address', \rex_request('create_new_shipping_address', 'int') == 1);
-$addresses         = $this->getVar('addresses', []);
-$orderAddress      = $this->getVar('Address');
-$back_url          = $this->getVar('back_url');
+$addNewAddress = isset(rex_post('FORM', 'array')['shop-shipping-address']);
 
 ?>
-<div class="addresses-grid margin-top margin-large-bottom">
+<div class="addresses-grid margin-top margin-large-bottom" data-address-toggle-container>
 
     <h2 class="heading large">###label.shipping_address###</h2>
 
-    <form method="POST" action="<?= rex_getUrl(null, null, array_merge($_GET, ['ts' => time()])) ?>" class="shipping-form">
-        <div class="address-toggle margin-top margin-bottom">
+    <form method="POST" action="<?= rex_getUrl(null, null, array_merge($_GET, ['ts' => time()])) ?>" class="shipping-form margin-top" id="dflt-shipping-address-wrapper">
+        <div class="address-wrapper">
+
+            <?php if (count($errors)): ?>
+                <div class="callout alert">
+                    - <?= implode('<br/>- ', $errors) ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="grid-x grid-margin-x shipping-addresses-wrapper margin-bottom" data-shipping-addresses>
+                <?php foreach ($addresses as $address): ?>
+                    <?php
+                    $addressData = $address->toAddressArray();
+                    ?>
+                    <div class="cell medium-6 large-4 margin-small-bottom">
+                        <div class="address-item-wrapper">
+                            <div class="custom-radio">
+                                <label>
+                                    <input type="radio" name="shipping-address" value="<?= $address->getId() ?>" <?= !$addNewAddress && $orderAddress->getId() == $address->getId() ? 'checked="checked"' : '' ?>>
+                                    <span class="radio"></span>
+                                    <?= implode('<br/>', $addressData); ?>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
             <div class="custom-checkbox">
                 <label class="custom-checkbox">
-                    <input type="checkbox" name="shipping_address_is_idem" value="1" <?= $useShAddress ? '' : 'checked="checked"' ?>
-                           onchange="Simpleshop.toggleShipping(this, '.shipping-form|.address-wrapper', true)"/>
-                    ###label.shipping_address_is_idem###
+                    <input type="checkbox" name="create_new_shipping_address" value="1" <?= $addNewAddress ? 'checked="checked"' : '' ?>
+                           onchange="Simpleshop.toggleShipping(this)"/>
+                    ###action.create_new_shipping_address###
                     <span class="checkbox"></span>
                 </label>
             </div>
         </div>
-        <div class="address-wrapper <?= $useShAddress ? '' : 'hide' ?>">
 
-            <?php if ($useShAddress && !empty($_POST)): ?>
-                <div class="callout alert">
-                    ###error.choose_shipping_address###
-                </div>
-            <?php endif; ?>
-
-            <?php if (count($addresses)): ?>
-                <h5>###label.use_existing_addredss###</h5>
-                <div class="grid-x grid-margin-x shipping-addresses-wrapper margin-bottom" data-shipping-addresses>
-                    <?php foreach ($addresses as $address): ?>
-                        <div class="cell medium-6 large-4">
-                            <div class="address-item-wrapper">
-                                <div class="custom-radio">
-                                    <label>
-                                        <input type="radio" name="shipping-address" value="<?= $address->getId() ?>" <?= $orderAddress->getId() == $address->getId() ? 'checked="checked"' : '' ?>>
-                                        <span class="radio"></span>
-
-                                        <?php
-                                        $this->setVar('address', $address);
-                                        $this->subfragment('simpleshop/customer/customer_area/address_data.php');
-                                        ?>
-
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif; ?>
-
-            <?php if (count($addresses)): ?>
-                <div class="custom-checkbox">
-                    <label class="custom-checkbox">
-                        <input type="checkbox" name="create_new_shipping_address" value="1" <?= $createNewShipping ? 'checked="checked"' : '' ?>
-                               onchange="Simpleshop.toggleShipping(this, '.address-wrapper|.new-address-wrapper')"/>
-                        ###action.create_new_shipping_address###
-                        <span class="checkbox"></span>
-                    </label>
-                </div>
-            <?php else: ?>
-                <h5>###action.create_new_shipping_address###</h5>
-            <?php endif; ?>
-            <div class="new-address-wrapper <?= count($addresses) && $createNewShipping != 1 ? 'hide' : '' ?>">
-                <?php
-                $fragment = new \rex_fragment();
-                $fragment->setVar('only_fields', true);
-                $fragment->setVar('show_save_btn', false);
-                echo $fragment->parse('simpleshop/customer/customer_area/address_form.php');
-                ?>
-            </div>
-        </div>
-
-        <div class="clearfix">
-            <div class="margin-top">
-                <a href="<?= $back_url ?>" class="button hollow">###action.go_back###</a>
-                <button type="submit" class="button float-right">###action.go_ahead###</button>
-            </div>
+        <div class="checkout-buttons <?= $addNewAddress ? 'hide' : '' ?>" data-address-toggle="existing-address">
+            <a href="<?= $back_url ?>" class="button hollow">###action.go_back###</a>
+            <button type="submit" name="action" value="apply-shipping-address" class="button">###action.go_ahead###</button>
         </div>
     </form>
+    <div class="new-address-wrapper margin-small-top <?= $addNewAddress ? '' : 'hide' ?>" data-address-toggle="new-address">
+
+        <?php
+        $this->setVar('Address', CustomerAddress::create());
+        $this->setVar('save_callback', function ($address) {
+            $Order = Session::getCurrentOrder();
+            $Order->setValue('shipping_address', $address);
+            $Order->setValue('shipping_address_id', $address->getId());
+            Session::setCheckoutData('Order', $Order);
+
+            $redirectUrl = html_entity_decode(rex_getUrl(null, null, array_merge($_GET, ['ts' => time()])));
+            \rex_response::sendCacheControl();
+            \rex_response::sendRedirect($redirectUrl);
+        });
+        $this->subfragment('simpleshop/customer/customer_area/shipping_address.php');
+        ?>
+    </div>
+
 </div>
