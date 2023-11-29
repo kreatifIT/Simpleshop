48bd14f0c15710c9803d3dd73a3605648ce24d4a
Philipp Parth <p.parth@kreatif.it>
2023-11-29 13:40:44 +0100

    discount tax calculation fixed | extensionPoints added

--- lib/abstract/Discount.php
+++ lib/abstract/Discount.php
@@ -20,75 +20,58 @@ abstract class Discount extends Model
 		if (!$Order) {
 			return false;
 		}
-		$discount    = 0;
-		$addDiscount = true;
+		$discount = 0;
 
-		$addDiscount = (bool)\rex_extension::registerPoint(
-			new \rex_extension_point('simpleshop.coupon.addDiscountCheck', $addDiscount, [
-				'Order'  => $Order,
-				'Coupon' => $this,
+		if ($name == 'manual_discount') {
+			$discount = $this->getValue('discount_value');
+		} elseif ($this->getValue('free_shipping')) {
+			$Order->setValue('shipping_costs', 0);
+		} elseif ($this->getValue('discount_value') > 0) {
+			$discount = $this->getValue('discount_value');
+		} elseif ($this->getValue('discount_percent') > 0) {
+			$discount = array_sum($gross_prices) / 100 * $this->getValue('discount_percent');
+		}
+
+		$discount = \rex_extension::registerPoint(
+			new \rex_extension_point('simpleshop.coupon.discount', $discount, [
+				'brut_prices' => &$gross_prices,
+				'discount'    => $this,
 			])
 		);
 
-		if ($addDiscount) {
-			$discountGrossPrice = (array)\rex_extension::registerPoint(
-				new \rex_extension_point('simpleshop.coupon.apply', $gross_prices, [
-					'Order'  => $Order,
-					'Coupon' => $this,
-				])
-			);
-
-			if ($name == 'manual_discount') {
-				$discount = $this->getValue('discount_value');
-			} elseif ($this->getValue('free_shipping')) {
-				$Order->setValue('shipping_costs', 0);
-			} elseif ($this->getValue('discount_value') > 0) {
-				$discount = $this->getValue('discount_value');
-			} elseif ($this->getValue('discount_percent') > 0) {
-				$discount = array_sum($discountGrossPrice) / 100 * $this->getValue('discount_percent');
-			}
-
-			if ($discount > 0 && $addDiscount) {
-				$discount = $discount - $this->applyToGrossPrices($discount, $gross_prices);
-			}
-			$this->setValue('value', $discount);
-
-			return $discount;
+		if ($discount > 0) {
+			$discount = $discount - $this->applyToGrossPrices($discount, $gross_prices);
 		}
+		$this->setValue('value', $discount);
+
+		return $discount;
 	}
 
 	public function applyToCart(&$brut_prices)
 	{
-		$discount    = 0;
-		$addDiscount = true;
-
-		$addDiscount = (bool)\rex_extension::registerPoint(
-			new \rex_extension_point('simpleshop.coupon.addDiscountCheck', $addDiscount, [
-				'Coupon' => $this,
+		$discount = 0;
+		if ($this->getValue('free_shipping')) {
+			// nothing to do?
+			$discount = 0;
+		} elseif ($this->getValue('discount_value') > 0) {
+			$discount = $this->getValue('discount_value');
+		} elseif ($this->getValue('discount_percent') > 0) {
+			$discount = array_sum($brut_prices) / 100 * $this->getValue('discount_percent');
+		}
+		
+		$discount = \rex_extension::registerPoint(
+			new \rex_extension_point('simpleshop.coupon.discount', $discount, [
+				'brut_prices' => &$brut_prices,
+				'discount'    => $this,
 			])
 		);
-		if ($addDiscount) {
-			$discountBrutPrice = (array)\rex_extension::registerPoint(
-				new \rex_extension_point('simpleshop.coupon.apply', $brut_prices, [
-					'Coupon' => $this,
-				])
-			);
-			if ($this->getValue('free_shipping')) {
-				// nothing to do?
-				$discount = 0;
-			} elseif ($this->getValue('discount_value') > 0) {
-				$discount = $this->getValue('discount_value');
-			} elseif ($this->getValue('discount_percent') > 0) {
-				$discount = array_sum($discountBrutPrice) / 100 * $this->getValue('discount_percent');
-			}
 
-			if ($discount > 0 && $addDiscount) {
-				$discount = $discount - $this->applyToGrossPrices($discount, $brut_prices);
-			}
-			$this->setValue('value', $discount);
-
-			return $discount;
+		if ($discount > 0) {
+			$discount = $discount - $this->applyToGrossPrices($discount, $brut_prices);
 		}
+		$this->setValue('value', $discount);
+
+		return $discount;
 	}
 
 	protected function applyToGrossPrices($_discount, &$gross_prices)
@@ -97,6 +80,7 @@ abstract class Discount extends Model
 		krsort($gross_prices);
 
 		foreach ($gross_prices as $tax => &$gross_price) {
+			$_discount   = $_discount + ($_discount * $tax / 100);
 			$gross_price = $this->calcPriceAndDiff($gross_price, $_discount);
 
 			if ($_discount <= 0) {
